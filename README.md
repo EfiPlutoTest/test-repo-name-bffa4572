169a6cf532578165fce944e1b4e9c3d40d56bbbd
plutointegration <user@example.com>
2022-05-31 11:28:34 +0000

    initial commit of Pluto Template files

---
+++ README.md
@@ -0,0 +1,6 @@
+
+
+# This repository has been configured with the following templates
+
+- Template using dependencies
+
---
+++ too_long.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "Trying to execute the script for too long"
+sleep 12
+echo "This should not be printed out"
